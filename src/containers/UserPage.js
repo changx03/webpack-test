bd0cbcfd67c7ca82818a45d71ed4baae71fd7f5b
Luke Chang <luke@csel.co.nz>
2018-04-11 15:40:27 +1200

    fixed babel

--- src/containers/UserPage.js
+++ src/containers/UserPage.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 export default class Users extends Component {
+  state = {
+    value: '',
+  };
+
   render() {
-    return(
+    return (
       <div>
         <h1>Users</h1>
+        <input
+          id="user-name"
+          type="text"
+          value={this.state.value}
+          onChange={this._onChange}
+        />
       </div>
     );
   }
+
+  _onChange = async e => {
+    await this.setState({
+      value: e.target.value,
+    });
+
+    console.log(this.state.value);
+  };
 }
